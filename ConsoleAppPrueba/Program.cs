f0a6965216b846cdd19135cef3569a7fef259b07
Jose Alberto <jagarcias@cifpvirgendegracia.com>
2020-09-25 10:03:43 +0200

    Revert "seguimos de pruebas"
    This reverts commit 4750ff27394acc79f40fc0c9a13bbb170e2fae53.

--- ConsoleAppPrueba/Program.cs
+++ ConsoleAppPrueba/Program.cs
@@ -15,7 +15,8 @@ namespace ConsoleAppPrueba
 
             DateTime ahora = DateTime.Now;
             Console.WriteLine(ahora.ToLongDateString());
-            Console.WriteLine("pruebaaaaaa");
+            Console.WriteLine(ahora.ToShortDateString());
+            Console.WriteLine(ahora.ToShortTimeString());
             Console.WriteLine(ahora.ToLongTimeString());
             Console.WriteLine(ahora.ToString("dd/MM/yyyy hh:mm:ss"));
         }
